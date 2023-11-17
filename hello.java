2d1eb1518d7866c0c7229dc18dbc065ec969cccb
Pawan Chandra Joshi <pawanpcj600@gmail.com>
2023-11-17 12:25:06 +0530

    Initial Commit

---
+++ hello.java
@@ -0,0 +1,10 @@
+/**
+ * hello
+ */
+public class hello {
+    public static void main(String arg[]) {
+        int num = 129;
+        float ff = 12.6f;
+        System.out.println("Hello World");
+    }
+}
