891c54b4f191c2c392a715e9ee086ac45b2c583c
Pawan Chandra Joshi <pawanpcj600@gmail.com>
2023-11-18 18:30:09 +0530

    Added 7-Encapsulation

---
+++ 7-Encapsulation/Encapsulation.java
@@ -0,0 +1,12 @@
+class Human {
+    int age;
+    String name;
+}
+
+public class Encapsulation {
+    public static void main(String[] args) {
+        Human obj = new Human();
+        obj.age = 12;
+        obj.name = "Daniel";
+    }
+}
